6e5e0230c843f23def213aa16b5a9b0b251edaa3
Glenn 'devalias' Grant <glenn@devalias.net>
2014-09-17 15:17:24 +1000

    Assignment 1 - Recfun

--- assignment-1-recfun/src/main/scala/recfun/Main.scala
+++ assignment-1-recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,6 @@
 package recfun
 import common._
+import scala.annotation.tailrec
 
 object Main {
   def main(args: Array[String]) {
@@ -14,15 +15,38 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 | c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec def loop(isOpen: Boolean, count: Int, chars: List[Char]): Boolean = {
+      if (chars.isEmpty) !isOpen && count == 0
+      else {
+        chars.head match {
+          case '(' => loop(true, count + 1, chars.tail)
+          case ')' => loop(false, count - 1, chars.tail)
+          case _ => loop(isOpen, count, chars.tail)
+        }
+      }
+    }
+    
+    loop(false, 0, chars)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    money match {
+      case _ if money < 0 => 0
+      case _ if money == 0 => 1
+      case _ if coins.isEmpty => 0
+      case _ => countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
+  }
 }
